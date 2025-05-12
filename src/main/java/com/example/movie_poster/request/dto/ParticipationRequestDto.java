711e241220a3861f9ba0ee01327e9da62e842186
Sayat86 <s.beysembayev@bk.ru>
2025-05-12 19:11:52 +0600

    correct participationRequestDto

--- src/main/java/com/example/movie_poster/request/dto/ParticipationRequestDto.java
+++ src/main/java/com/example/movie_poster/request/dto/ParticipationRequestDto.java
@@ -1,6 +1,8 @@
 package com.example.movie_poster.request.dto;
 
 import com.example.movie_poster.request.RequestState;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,9 +15,14 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ParticipationRequestDto {
+    @NotNull
     private Integer requester;
+    @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime created;
+    @NotNull
     private Integer id;
+    @NotNull
     private Integer event;
     private RequestState status;
 }
